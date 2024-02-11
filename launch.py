0bda2e6e12337cc1ba3ef22f1a4bfdbb9eb24d57
MaxG7855 <96419168+MaxG7855@users.noreply.github.com>
2024-02-11 16:53:18 +0000

    Update launch.py

--- launch.py
+++ launch.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageTk
 import os
 import subprocess
 
-code = 'https://raw.githubusercontent.com/MaxG7855/ee/main/program.py'
+code = 'https://raw.githubusercontent.com/MaxG7855/ee/main/program.txt'
 
 response = urllib.request.urlopen(code)
 data = response.read()
